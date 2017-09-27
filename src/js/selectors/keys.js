8eb4129959de6cea5fa4c81b3a33d218730fa623
ramfox <klhuizinga@gmail.com>
2017-09-27 17:45:30 -0400

    Missed adding this file on first pass of porting over datatogether's MetadataEditor component

---
+++ src/js/selectors/keys.js
@@ -0,0 +1,22 @@
+
+// default key looks for a key named "default key", falling back to any
+// available key
+export function selectDefaultKey (state) {
+  const { keys } = state.entities
+  return Object.keys(keys).map(id => keys[id]).reduce((key, a) => {
+    if (key && key.name == 'default key') {
+      return key
+    }
+    return a
+  }, {})
+}
+
+export function selectDefaultKeyId (state) {
+  const key = selectDefaultKey(state)
+  return key.sha256 || ''
+}
+
+export function selectKeys (state) {
+  const { keys } = state.entities
+  return Object.keys(keys).map(id => keys[id])
+}
