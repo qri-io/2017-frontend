a1b270b85eed6296da9a91f9848bc5abd924a1b0
ramfox <klhuizinga@gmail.com>
2017-10-11 12:00:30 -0400

    Dataset container - mapStateToProps moved to anonymous function in connect

--- app/containers/Dataset.js
+++ app/containers/Dataset.js
@@ -10,42 +10,37 @@ import { selectSessionUser } from '../selectors/session'
 
 import Dataset from '../components/Dataset'
 
-function mapStateToProps (state, ownProps) {
-  const path = ownProps.path || `/${ownProps.params.splat}`
+const DatasetContainer = connect(
+  (state, ownProps) => {
+    const path = ownProps.path || `/${ownProps.params.splat}`
 
-  const user = selectSessionUser(state)
-  const results = state.results[state.console.query.statement]
+    const user = selectSessionUser(state)
+    const results = state.results[state.console.query.statement]
 
-  let permissions = {
-    edit: false,
-    migrate: false,
-    change: false
+    let permissions = {
+      edit: false,
+      migrate: false,
+      change: false
+    }
+    if (user && user.username === ownProps.params.user) {
+      permissions.migrate = true
+      permissions.change = true
+    }
+    return Object.assign({
+      path,
+      datasetRef: selectDataset(state, path),
+      data: selectDatasetData(state, path),
+      results,
+      permissions
+    }, state.console, ownProps)
+  }, {
+    setQuery,
+    runQuery,
+    downloadQuery,
+    downloadDataset,
+    deleteDataset,
+    loadDatasetData
   }
-
-  if (user && user.username === ownProps.params.user) {
-    permissions.migrate = true
-    permissions.change = true
-  }
-
-  return Object.assign({
-    path,
-
-    datasetRef: selectDataset(state, path),
-    data: selectDatasetData(state, path),
-    results,
-    permissions
-
-  }, state.console, ownProps)
-}
-
-const DatasetContainer = connect(mapStateToProps, {
-  setQuery,
-  runQuery,
-  downloadQuery,
-  downloadDataset,
-  deleteDataset,
-  loadDatasetData
-  // loadDatasetReadme,
-})(Dataset, 'Dataset')
+)(Dataset, 'Dataset')
 
 export default DatasetContainer
