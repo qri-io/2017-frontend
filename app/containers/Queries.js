091c9d68ef98941591a39e80f313732ff071ff84
ramfox <klhuizinga@gmail.com>
2017-10-25 16:26:48 -0400

    Starting to refactor selectors. Anything that pulls from pagination should use the pagination selectors

--- app/containers/Queries.js
+++ app/containers/Queries.js
@@ -2,20 +2,21 @@ import { connect } from 'react-redux'
 
 import { loadQueries, setQuery } from '../actions/query'
 import { setTopPanel } from '../actions/console'
-import { selectQueries, selectQueriesPageCount, selectQueriesFetchedAll, selectQueriesIsFetching } from '../selectors/query'
+import { selectQueries } from '../selectors/query'
+import { selectIsFetching, selectPageCount, selectFetchedAll } from '../selectors/pagination'
 
 import Queries from '../components/Queries'
 
 const QueriesContainer = connect(
   (state, ownProps) => {
     const pagination = state.pagination.popularQueries
-    const paginationSection = ''
-    const searchString = ''
+    const paginationSection = 'popularQueries'
+    const searchString = 'popularQueries'
     return Object.assign({
       queries: selectQueries(state, paginationSection, searchString),
-      loading: selectQueriesIsFetching(state, paginationSection, searchString),
-      nextPage: selectQueriesPageCount(state, paginationSection, searchString) + 1,
-      fetchedAll: selectQueriesFetchedAll(state, paginationSection, searchString)
+      loading: selectIsFetching(state, paginationSection, searchString),
+      nextPage: selectPageCount(state, paginationSection, searchString) + 1,
+      fetchedAll: selectFetchedAll(state, paginationSection, searchString)
     }, ownProps)
   }, {
     loadQueries,
