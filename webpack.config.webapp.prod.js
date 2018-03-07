f1034af3e38c311b7ed528ff70f8a18f02e8928c
b5 <sparkle_pony_2000@qri.io>
2018-03-07 15:25:40 -0500

    chore(Webapp build): make webapp build output a single file
    Ok, for the sake of simplicity we're currently going to have the webapp compile
    down to a single file so we can ship it over the d.web as a self-contained app.
    
    The problem here stems from not being able to get webpack fetches to happen in the
    context of a directory inside a hash-path. We either need to proxy requests for certian
    files to a hash-directory on the webapp server in qri-io/qri, or find some way to
    mess with webpack's runtime "import" calls
    
    In the future we should *absolutely* imporove on this situation, but for now this works.

--- webpack.config.webapp.prod.js
+++ webpack.config.webapp.prod.js
@@ -9,8 +9,6 @@ import MinifyPlugin from 'babel-minify-webpack-plugin'
 
 CheckNodeEnv('production')
 
-const publicPath = `/`
-
 export default merge.smart(baseConfig, {
   target: 'web',
   mode: 'production',
@@ -18,20 +16,18 @@ export default merge.smart(baseConfig, {
   entry: path.join(__dirname, 'lib/index.js'),
 
   output: {
-    publicPath,
+    publicPath: '/',
     path: path.join(__dirname, '/dist/web'),
     filename: '[name].js',
     libraryTarget: 'umd'
   },
 
   plugins: [
-    new MinifyPlugin({}, {
-      sourceMap: null
-    }),
+    new MinifyPlugin({}, { sourceMap: null }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
       '__BUILD__': {
-        'MODE': JSON.stringify(process.env.NODE_ENV || 'development'),
+        'MODE': JSON.stringify(process.env.NODE_ENV || 'production'),
         'BASE_URL': JSON.stringify('http://localhost:2503'),
         'API_URL': JSON.stringify('http://localhost:2503'),
         'STATIC_ASSETS_URL': JSON.stringify('http://localhost:2503'),
@@ -90,14 +86,13 @@ export default merge.smart(baseConfig, {
       // WOFF Font
       {
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        // use: {
-        //   loader: 'url-loader',
-        //   options: {
-        //     limit: 10000,
-        //     mimetype: 'application/font-woff'
-        //   }
-        // }
-        use: 'file-loader'
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+            mimetype: 'application/font-woff'
+          }
+        }
       },
       // WOFF2 Font
       {
@@ -105,7 +100,7 @@ export default merge.smart(baseConfig, {
         use: {
           loader: 'url-loader',
           options: {
-            limit: 10000,
+            limit: 100000,
             mimetype: 'application/font-woff'
           }
         }
@@ -113,31 +108,35 @@ export default merge.smart(baseConfig, {
       // TTF Font
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        // use: {
-        //   loader: 'url-loader',
-        //   options: {
-        //     limit: 10000,
-        //     mimetype: 'application/octet-stream'
-        //   }
-        // }
-        use: 'file-loader'
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 500000,
+            mimetype: 'application/octet-stream'
+          }
+        }
       },
       // EOT Font
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: 'file-loader'
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+            mimetype: 'application/vnd.ms-fontobject'
+          }
+        }
       },
       // SVG Font
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        // use: {
-        //   loader: 'url-loader',
-        //   options: {
-        //     limit: 10000,
-        //     mimetype: 'image/svg+xml'
-        //   }
-        // }
-        use: 'file-loader'
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+            mimetype: 'image/svg+xml'
+          }
+        }
       },
       // Common Image Formats
       {
